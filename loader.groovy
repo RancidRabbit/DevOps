634920eb296c78c7afbc504253c2ddda0b6073ad
fright <asumerkin@bk.ru>
2023-05-24 19:45:09 +0300

    ...

--- loader.groovy
+++ loader.groovy
@@ -5,7 +5,7 @@ def buildApp(){
 def buildAndDeployImage(){
     withCredentials([usernamePassword(credentialsId: 'jenkins-nexus', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t 209.38.249.127:8083/simple-java:1.1 .'
-        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin 209.38.249.127:8083"
+        sh "echo $PASS | docker login -u $USER --password-stdin 209.38.249.127:8083"
         sh 'docker push 209.38.249.127:8083/simple-java:1.1'
     }
 }
