c7d98a80dde1868a4e0737693e0f7d07145c876a
fright <asumerkin@bk.ru>
2023-05-27 20:39:44 +0300

    ...

--- loader.groovy
+++ loader.groovy
@@ -6,7 +6,7 @@ def buildApp(){
 }
 
 def buildAndDeployImage(){
-    withCredentials([usernamePassword(credentialsId: 'ff48a99f-bb74-4f30-a5e7-7a98a2cffed3', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    withCredentials([usernamePassword(credentialsId: 'nexus-user', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t 209.38.249.127:8083/simple-java:1.1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin 209.38.249.127:8083"
         sh 'docker push 209.38.249.127:8083/simple-java:1.1'
