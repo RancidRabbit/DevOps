beea91c9d56ab36b4fbafb3a3ef03900de2aa0a7
fright <asumerkin@bk.ru>
2023-05-29 17:57:46 +0300

    ...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,30 @@
 public class Main {
 
+    /**
+     * Write a function that takes an array of numbers (integers for the tests) and a target number.
+     * It should find two different items in the array that, when added together, give the target value.
+     * The indices of these items should then be returned in a tuple / list (depending on your language)
+     * like so: (index1, index2).
+     * @author : Sumerkin A.
+     */
 
-    static String str = "Super Awesome Java App";
-
-
-
-    public static void main(String[] args) {
-
-        System.out.println(str);
-
-
+    public static int[] twoSum(int[] numbers, int target) {
+        boolean isFind = false;
+        int[] result = new int[2];
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                    isFind = true;
+                    break;
+                }
+                if(isFind) {
+                    break;
+                }
+            }
+        }
+        return result;
     }
 
 
