053a841b6b7fcf498d4cb4bb17c7c025b7140443
fright <asumerkin@bk.ru>
2023-05-30 17:15:54 +0300

    ...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
      * It should find two different items in the array that, when added together, give the target value.
      * The indices of these items should then be returned in a tuple / list (depending on your language)
      * like so: (index1, index2).
-     * @author : Sumerkin A.
+     *
      */
 
     public static int[] twoSum(int[] numbers, int target) {
